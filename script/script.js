0886a77f48fb8a8557bbe3f505715f452fc49dbe
Marina Morogova <marina_0880@mail.ru>
2023-10-09 13:20:10 +0700

    Вёрстка

---
+++ script/script.js
@@ -0,0 +1,31 @@
+const buttonParams = document.querySelector('.parameters');
+const paramList = document.querySelector('.parameters__list');
+
+
+buttonParams.addEventListener('click', function() {
+    paramList.style.display = 'block';
+});
+
+// $(function() {
+//     $('.parameters').on('click', function ()
+//     {
+//         $('.parameters__list').show(500);
+//         $('.parameters__list').on('mouseleave', function () 
+//         {
+//             $('.parameters__list').hide(500);
+//         });
+//     });
+// });
+
+
+
+// $(function() {
+//     $('button').on('click', function ()
+//     {
+//         $('.menuList').show(500);
+//         $('.miniMenu').on('mouseleave', function() 
+//         {
+//             $('.menuList').hide(500);
+//         });
+//     });
+// });
