9ac4c20a417aa632920e2682a62d078ca36c00dc
Marina Morogova <marina_0880@mail.ru>
2023-10-14 09:51:16 +0700

    Изменено положение стрелок слайдера через js (на 768), убран абсолют

--- script/script.js
+++ script/script.js
@@ -45,3 +45,32 @@ function closeParamsMenu(event) {
 
 paramsBtn.addEventListener('mouseover', openParamsMenu);
 paramsList.addEventListener('mouseleave', closeParamsMenu);
+
+//Arrows (768px)
+
+let leftArrowDiv = document.querySelector('#arrow-left');
+let rightArrowDiv = document.querySelector('#arrow-right');
+let slideImgBox = document.querySelector('#slide-img-box');
+let sliderBox = document.querySelector('#slider-container');
+
+
+if ((window.matchMedia("(min-width: 481px)").matches) & (window.matchMedia("(max-width: 768px)").matches)) {
+
+    slideImgBox.before(leftArrowDiv);
+    slideImgBox.after(rightArrowDiv);
+
+}
+
+if (window.matchMedia("(max-width: 480px)").matches) {
+
+    sliderBox.before(leftArrowDiv);
+    sliderBox.after(rightArrowDiv);
+    console.log('yes');
+}
+
+// if (window.matchMedia("(max-width: 768px)").matches) {
+
+//     slideImgBox.before(leftArrowDiv);
+//     slideImgBox.after(rightArrowDiv);
+
+// }
